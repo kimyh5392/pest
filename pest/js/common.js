3223c937727918715594ac7003e491648dce2416
Kim Yong Hwan <mk.kimyh@gmail.com>
2022-08-24 09:00:13 +0900

    TEST main

--- pest/js/common.js
+++ pest/js/common.js
@@ -1,3 +1,10 @@
 (function($){
-    
+    // gnb nav 관련
+    $(document).on('mouseenter', '.gnb-menu > li > a', function(){
+        $('.gnb-menu > li').removeClass('active');
+        $(this).parent().addClass('active');
+    });
+    $(document).on('mouseleave', '.gnb-menu', function(){
+        $('.gnb-menu > li').removeClass('active');
+    });
 })(jQuery);
