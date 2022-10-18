df1d8b511c4c0fee33509563b6f2a2597a9112a2
Kim Yong Hwan <mk.kimyh@gmail.com>
2022-10-18 11:51:31 +0900

    mobile 마이페이지 작업중 #1 community quick-banner 수정사항 적용

---
+++ content/pest/js/quick_banner_sub.js
@@ -0,0 +1,14 @@
+(function($){
+    // quick-banner
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() >= 500) {
+            $('.quick-banner').stop().animate({
+                top: $(this).scrollTop() + 200
+            }, 'slow');
+        } else {
+            $('.quick-banner').stop().animate({
+                top: 684
+            }, 'slow');
+        }
+    });
+})(jQuery);
--- content/pest/js/sub.js
+++ content/pest/js/sub.js
@@ -1,16 +1,4 @@
 (function($){
-    // quick-banner
-    $(window).on('scroll', function() {
-        if ($(this).scrollTop() >= 500) {
-            $('.quick-banner').stop().animate({
-                top: $(this).scrollTop() + 200
-            }, 'slow');
-        } else {
-            $('.quick-banner').stop().animate({
-                top: 374
-            }, 'slow');
-        }
-    });
     
     // modal 
     $(document).on('click','.btn-modal', function(e){
